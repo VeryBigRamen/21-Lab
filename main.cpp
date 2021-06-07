a03396071cd418fc6500452a0e178a8de0550c1e
VeryBigRamen <61871678+VeryBigRamen@users.noreply.github.com>
2021-06-07 20:41:58 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "graphdesigner.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QgraphDesigner w;
+    w.show();
+
+    return a.exec();
+}
